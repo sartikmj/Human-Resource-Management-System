da8f8c388190c02d2f9e0aa2dbcc4774e8eec770
sartikmj <sartikmj@gmail.com>
2025-07-20 01:09:49 +0530

    setup some simple routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import EmployeeDetails from './components/EmployeeDetails.jsx';
+import EmployeeManagementApp from './components/EmployeeManagementApp.jsx';
 import './App.css';
-
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div>
-      
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Navigate to="employee" />} />
+            <Route path='/employee' element={<EmployeeManagementApp />} />
+            <Route path='/employee/:id' element={<EmployeeDetails />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/EmployeeManagementApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployeeManagementApp = () => {
+  return (
+    <div>EmployeeManagementApp</div>
+  )
+}
+
+export default EmployeeManagementApp
